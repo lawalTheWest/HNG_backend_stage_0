5462c01f71395a515f3baf0bed14a8b9b429e87c
lawal tajudeen ogunsola <lwltjdn@gmail.com>
2025-10-23 12:53:06 +0100

    updated to add a default secret key '

--- backend_profile_project/settings.py
+++ backend_profile_project/settings.py
@@ -21,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = 'django-insecure-kv(&r%kpugea*0=ome$7gor32-u&-bey=3n7wezs50*acc+(13'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.railway.app', '127.0.0.1', 'localhost']
+# ALLOWED_HOSTS = ['.railway.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
